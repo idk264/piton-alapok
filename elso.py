c46ced12e86036144d9c62ef2fb2733421e60cd4
idk264 <csomos.andras.istvan@szbiszeged.hu>
2022-09-19 10:36:04 +0200

    fjsfhjfh

--- elso.py
+++ elso.py
@@ -2,3 +2,11 @@ from os import system
 
 system("cls")
 
+#1. feladat: orog alapf.2
+
+#explicit típuskonverzió
+szam = int(input("kérek egy számot: "))
+kiir = f"a szám: {szam} és kétszerese: {2 * szam}"
+
+
+print(kiir)
---
+++ feladatok.py
@@ -0,0 +1,9 @@
+from os import system
+import sys
+
+system("cls")
+
+#2.feladat: prog_alapf.3
+
+nev = input("kérem a nevedet: ")
+koszones = f"Hello {nev}!"
